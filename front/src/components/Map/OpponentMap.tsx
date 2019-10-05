1a87c6fd61cef69d6b906783f929ca546a7d7f15
Arnaud Huc <ahuc@oui.sncf>
2019-10-05 19:34:21 +0200

    Display game map

--- front/src/components/Map/OpponentMap.tsx
+++ front/src/components/Map/OpponentMap.tsx
@@ -4,13 +4,13 @@ import * as React from 'react';
 const OpponentMap: React.FC = function() {
   return(
     <div className='gameMap gameMap--opponent'>
-      <div className='gameMap__place'>gameMap__place</div>
-      <div className='gameMap__melee'>gameMap__melee</div>
-      <div className='gameMap__permanent'>gameMap__permanent</div>
-      <div className='gameMap__caster'>gameMap__caster</div>
-      <div className='gameMap__discard'>gameMap__discard</div>
-      <div className='gameMap__mana'>gameMap__mana</div>
-      <div className='gameMap__deck'>gameMap__deck</div>
+      <div className='gameMap__place'></div>
+      <div className='gameMap__melee'></div>
+      <div className='gameMap__permanent'></div>
+      <div className='gameMap__caster'></div>
+      <div className='gameMap__discard'></div>
+      <div className='gameMap__mana'></div>
+      <div className='gameMap__deck'></div>
     </div>
   )
 }
