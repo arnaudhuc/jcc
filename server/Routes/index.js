1686c35f6f39a2835dbb78ead5086f446e6b59e0
Arnaud Huc <ahuc@oui.sncf>
2019-12-03 18:10:52 +0100

    Add Kotlin + Maven

--- server/Routes/index.js
+++
@@ -1,3 +0,0 @@
-export { default as cardRouter } from './cardRouter';
-export { default as deckRouter } from './deckRouter';
-export { default as playerRouter } from './playerRouter';
--- server/models/index.js
+++
@@ -1,3 +0,0 @@
-export { default as PlayerModel } from './playerModel';
-export { default as DeckModel } from './deckModel';
-export { default as CardModel } from './cardModel';
